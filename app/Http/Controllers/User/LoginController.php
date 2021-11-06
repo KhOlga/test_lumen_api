463c10a3679b24b87f9add76cf0ad2110ae6c36d
Olha Kharabet <kharabet.olga@gmail.com>
2021-11-06 14:42:57 +0200

    Added auth routes, empty controllers

---
+++ app/Http/Controllers/User/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+class LoginController extends \App\Http\Controllers\Controller
+{
+	public function create()
+	{
+		//
+	}
+
+	public function store()
+	{
+		//
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,30 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you can register all of the API routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+use App\Http\Controllers\User\LoginController;
+use App\Http\Controllers\User\RegisterUserController;
+
+$router->group(['prefix' => 'api/v1', 'as' => 'api.v1', 'namespace' => 'API\\V1'], function () use ($router) {
+	$router->group(['prefix' => 'user', 'as' => 'user', 'namespace' => 'User', 'middleware' => 'guest'],
+		function () use ($router) {
+			$router->get('register', [RegisterUserController::class, 'create', 'as' => 'create']);
+			$router->post('register', [RegisterUserController::class, 'store', 'as' => 'store']);
+
+			$router->get('sign-in', [LoginController::class, 'create', 'as' => 'login']);
+
+			$router->post('sign-in', [LoginController::class, 'store', 'as' => 'login_store']);
+
+			$router->get('/', function () use ($router) {
+				return $router->app->version();
+			});
+	});
 });
