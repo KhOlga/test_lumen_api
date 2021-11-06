412f3dee0db720365e3ce4bb7cd63696b156803b
Olha Kharabet <kharabet.olga@gmail.com>
2021-11-06 23:39:42 +0200

    Code review

--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,20 @@ use App\Http\Controllers\User\RegisterUserController;
 |
 */
 
-$router->group(['prefix' => 'api', 'as' => 'api.v1', 'namespace' => 'API'], function () use ($router) {
+$router->group(['prefix' => 'api', 'as' => 'api', 'namespace' => 'API'], function () use ($router) {
 	$router->group(['prefix' => 'user', 'as' => 'user', 'namespace' => 'User', 'middleware' => 'guest'],
 		function () use ($router) {
-			$router->get('register', [RegisterUserController::class, 'create', 'as' => 'create']);
-			$router->post('register', [RegisterUserController::class, 'store', 'as' => 'store']);
+			$router->get('register', ['uses' => 'RegisterUserController@create', 'as' => 'create']);
+			$router->post('register', ['uses' => 'RegisterUserController@store', 'as' => 'store']);
 
-			$router->get('sign-in', [LoginController::class, 'create', 'as' => 'login']);
-			$router->post('sign-in', [LoginController::class, 'store', 'as' => 'login_store']);
+			$router->get('sign-in', [ 'uses' => 'LoginController@create', 'as' => 'login']);
+			$router->post('sign-in', [ 'uses' => 'LoginController@store', 'as' => 'login_store']);
 
 			$router->post('recover-password', [
-				RecoverPasswordController::class, 'recoverPassword', 'as' => 'recover_password'
+				'uses' => 'RecoverPasswordController@recoverPassword', 'as' => 'recover_password'
 			]);
 
-			$router->get('companies', [CompanyController::class, 'index', 'as' => 'index']);
-			$router->post('companies', [CompanyController::class, 'store', 'as' => 'store']);
+			$router->get('companies', ['uses' => 'CompanyController@index', 'as' => 'index']);
+			$router->post('companies', ['uses' => 'CompanyController@store', 'as' => 'store']);
 	});
 });
